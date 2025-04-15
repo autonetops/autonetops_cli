8e14ddb183de303896e4baa48aacc8ada0b549c5
pdamasceno <pedro.damasceno@hotmail.com>
2025-04-15 16:06:56 +0200

    update

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     include_package_data=True,
     install_requires=[
         'click==8.1.7',
+        'PyYAML==6.0.2',
+        'netmiko==4.4.0',
     ],
     entry_points='''
         [console_scripts]
