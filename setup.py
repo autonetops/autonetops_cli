f78365d13a975398fa20b2d5ad18916ec51ed744
pdamasceno <pedro.damasceno@hotmail.com>
2025-04-27 09:17:00 +0200

    update

--- setup.py
+++ setup.py
@@ -2,18 +2,28 @@ from setuptools import setup, find_packages
 
 setup(
     name='autonetops',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click==8.1.7',
-        'PyYAML==6.0.2',
-        'netmiko==4.4.0',
-        'Jinja2==3.1.2',
-        'rich==13.4.0',
+        'click>=8.1.7',
+        'PyYAML>=6.0',
+        'netmiko>=4.4.0',
+        'Jinja2>=3.1.2',
+        'rich>=13.4.0',
     ],
-    entry_points='''
-        [console_scripts]
-        autonetops=autonetops.autonetops:cli
-    ''',
+    entry_points={
+        'console_scripts': [
+            'autonetops=autonetops.autonetops:cli',
+        ],
+    },
+    author='Pedro C Damasceno',
+    description='Autonetops CLI for labs and exercises',
+    url='https://github.com/autonetops/autonetops_cli',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
 )
